6d4541cf5a0e8d8220e13515531071e24de90866
bhargavich-998 <bhargavicheedella998@gmail.com>
2025-12-10 21:24:04 +0530

    Fix Chrome CI flags

--- conftest.py
+++ conftest.py
@@ -28,7 +28,7 @@ logging.basicConfig(
 def driver():
     chrome_options = Options()
     #chrome_options.add_argument("--guest")
-    #chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
