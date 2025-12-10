553bd025c0aa979d2a926ac5deef3785fbc7171b
bhargavich-998 <bhargavicheedella998@gmail.com>
2025-12-10 19:35:14 +0530

    Fix Chrome CI flags

--- conftest.py
+++ conftest.py
@@ -27,7 +27,15 @@ logging.basicConfig(
 @pytest.fixture
 def driver():
     chrome_options = Options()
-    chrome_options.add_argument("--guest")
+    #chrome_options.add_argument("--guest")
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--remote-debugging-port=9222")
     driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     yield driver
